daf37d4653d42ed95810f538d049cc24f90edd2e
Nguyen_Anh_Tuan <banhmysv@gmail.com>
2018-01-02 16:13:54 +0700

    update

---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { Chart } from 'angular-highcharts';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  constructor() {
+  }
+  chart = new Chart({
+    chart: {
+      type: 'line'
+    },
+    title: {
+      text: 'Linechart'
+    },
+    credits: {
+      enabled: false
+    },
+    series: [{
+      name: 'Line 1',
+      data: [1, 2, 3, 4, 5]
+    }]
+  });
+
+// add point to chart serie
+add() {
+  this.chart.addPoint(Math.floor(Math.random() * 10000));
+}
+}
